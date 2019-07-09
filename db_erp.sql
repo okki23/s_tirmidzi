94026b247ace42b9bc4041fbbee832a328bcdba6
okki23 <okkisetyawan@gmail.com>
2019-07-09 16:18:49 +0700

    update db

---
+++ db_erp.sql
@@ -0,0 +1,232 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100316
+ Source Host           : localhost:3306
+ Source Schema         : db_erp
+
+ Target Server Type    : MySQL
+ Target Server Version : 100316
+ File Encoding         : 65001
+
+ Date: 09/07/2019 16:18:26
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for m_barang
+-- ----------------------------
+DROP TABLE IF EXISTS `m_barang`;
+CREATE TABLE `m_barang`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_barang` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `qty` int(10) NULL DEFAULT NULL,
+  `id_jenis` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of m_barang
+-- ----------------------------
+INSERT INTO `m_barang` VALUES (3, 'Kue Kering', 10, 3);
+INSERT INTO `m_barang` VALUES (5, 'Odol', 10, 2);
+INSERT INTO `m_barang` VALUES (6, 'Batu Bata', 10, 4);
+
+-- ----------------------------
+-- Table structure for m_jabatan
+-- ----------------------------
+DROP TABLE IF EXISTS `m_jabatan`;
+CREATE TABLE `m_jabatan`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_jabatan` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `gapok` int(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of m_jabatan
+-- ----------------------------
+INSERT INTO `m_jabatan` VALUES (1, 'IT Head Dept', 6000000);
+INSERT INTO `m_jabatan` VALUES (3, 'HR Manager', 6800000);
+
+-- ----------------------------
+-- Table structure for m_jenis
+-- ----------------------------
+DROP TABLE IF EXISTS `m_jenis`;
+CREATE TABLE `m_jenis`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_jenis` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of m_jenis
+-- ----------------------------
+INSERT INTO `m_jenis` VALUES (2, 'Basah');
+INSERT INTO `m_jenis` VALUES (3, 'Kering');
+INSERT INTO `m_jenis` VALUES (4, 'Padat');
+
+-- ----------------------------
+-- Table structure for m_karyawan
+-- ----------------------------
+DROP TABLE IF EXISTS `m_karyawan`;
+CREATE TABLE `m_karyawan`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nip` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `nama` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `alamat` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `telp` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `email` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `id_jabatan` int(10) NULL DEFAULT NULL,
+  `id_status` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of m_karyawan
+-- ----------------------------
+INSERT INTO `m_karyawan` VALUES (1, '354563', 'Okki Setyawan', 'Jl.Bintara', '02188345', 'okkisetyawan@gmail.com', 1, 2);
+INSERT INTO `m_karyawan` VALUES (2, '8324', 'Tarmizi', 'Jl. Buaran', '02194783457', 'mi@mail.com', 1, 2);
+
+-- ----------------------------
+-- Table structure for m_status
+-- ----------------------------
+DROP TABLE IF EXISTS `m_status`;
+CREATE TABLE `m_status`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_status` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `tunjangan` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of m_status
+-- ----------------------------
+INSERT INTO `m_status` VALUES (2, 'Menikah', 1000000);
+INSERT INTO `m_status` VALUES (5, 'Janda', 2000000);
+
+-- ----------------------------
+-- Table structure for m_supplier
+-- ----------------------------
+DROP TABLE IF EXISTS `m_supplier`;
+CREATE TABLE `m_supplier`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_supplier` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `alamat` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `telp` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `email` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of m_supplier
+-- ----------------------------
+INSERT INTO `m_supplier` VALUES (1, 'PT.Angkasa', 'Jl.Nangka', '02187485', 'info@angkasa.com');
+INSERT INTO `m_supplier` VALUES (2, 'PT.Megah Jaya', 'Jl.Gunawan', '0218478543', 'gun@mail.com');
+INSERT INTO `m_supplier` VALUES (3, 'PT.Sarana Cipta Utama', 'Jl.Waru', '021843587', 'info@mail.com');
+
+-- ----------------------------
+-- Table structure for m_user
+-- ----------------------------
+DROP TABLE IF EXISTS `m_user`;
+CREATE TABLE `m_user`  (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `id_karyawan` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of m_user
+-- ----------------------------
+INSERT INTO `m_user` VALUES (1, 'admin', 'YQ==', NULL);
+
+-- ----------------------------
+-- Table structure for t_payroll
+-- ----------------------------
+DROP TABLE IF EXISTS `t_payroll`;
+CREATE TABLE `t_payroll`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pegawai` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `periode` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `date_generate` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_payroll
+-- ----------------------------
+INSERT INTO `t_payroll` VALUES (5, '1', '201801', '2019-07-08 10:05:01');
+INSERT INTO `t_payroll` VALUES (6, '2', '201801', '2019-07-08 10:05:01');
+
+-- ----------------------------
+-- Table structure for t_po
+-- ----------------------------
+DROP TABLE IF EXISTS `t_po`;
+CREATE TABLE `t_po`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `no_po` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `date_assign` date NULL DEFAULT NULL,
+  `user_assign` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_po_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `t_po_detail`;
+CREATE TABLE `t_po_detail`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id_po` int(10) NULL DEFAULT NULL,
+  `id_pricelist` int(10) NULL DEFAULT NULL,
+  `qty` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_pricelist
+-- ----------------------------
+DROP TABLE IF EXISTS `t_pricelist`;
+CREATE TABLE `t_pricelist`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id_barang` int(10) NULL DEFAULT NULL,
+  `id_supplier` int(10) NULL DEFAULT NULL,
+  `harga` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_pricelist
+-- ----------------------------
+INSERT INTO `t_pricelist` VALUES (7, 3, 2, 9000);
+
+-- ----------------------------
+-- Table structure for t_so
+-- ----------------------------
+DROP TABLE IF EXISTS `t_so`;
+CREATE TABLE `t_so`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `no_so` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `date_assign` date NULL DEFAULT NULL,
+  `user_assign` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_so_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `t_so_detail`;
+CREATE TABLE `t_so_detail`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id_so` int(10) NULL DEFAULT NULL,
+  `id_pricelist` int(10) NULL DEFAULT NULL,
+  `qty` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
